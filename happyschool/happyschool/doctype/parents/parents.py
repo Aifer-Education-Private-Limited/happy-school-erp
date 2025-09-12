fbc67c170202950af88ab4d157c01c2101e1c00f
esra thanoona <esrathanoonaaifer@gmail.com>
2025-09-12 17:20:56 +0530

    fix:student signup api edits

--- happyschool/happyschool/doctype/parents/parents.py
+++ happyschool/happyschool/doctype/parents/parents.py
@@ -8,3 +8,16 @@ from frappe.model.naming import make_autoname
 class Parents(Document):
     def autoname(self):
         self.name = make_autoname("PT-.YYYY.-.#####")
+
+@frappe.whitelist()
+def createparent(parent_id, first_name=None, last_name=None, mobile_number=None):
+    student = frappe.get_doc({
+        "doctype": "Students",
+        "parent_id": parent_id,
+        "first_name": first_name,
+        "last_name": last_name,
+        "mobile_number": mobile_number
+    })
+    student.insert(ignore_permissions=True)
+    frappe.db.commit()
+    return student.name
