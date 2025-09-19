039296fb0b1cb984d976f9f32a7e9ce1900122ed
akilsalamnkaifer <akilsalamnkaifer@gmail.com>
2025-09-19 17:42:14 +0530

    added material type

--- happyschool/api/v4_api.py
+++ happyschool/api/v4_api.py
@@ -55,7 +55,8 @@ def get_student_materials():
                     "files": material.files,  # Assuming files are linked correctly in the material doctype
                     "submitted_date": material.submitted_date,
                     "session_id": material.session_id,
-                    "student_id": material.student_id
+                    "student_id": material.student_id,
+                    "type": "material"
                 }
 
                 topic_dict[material.topic][material.subtopic].append(material_data)
