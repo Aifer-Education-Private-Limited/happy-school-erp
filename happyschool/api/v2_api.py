3ae0880de10d72844563047dda587ae1f8808e23
akilsalamnkaifer <akilsalamnkaifer@gmail.com>
2025-09-23 18:38:16 +0530

    save mobile number in transaction

--- happyschool/api/v2_api.py
+++ happyschool/api/v2_api.py
@@ -234,12 +234,12 @@ def checkout(
         frappe.db.sql("""
             INSERT INTO `tabHS Transactions`
             (name, txn_id, amount, payable, products, email, customer_name, parent_id, time, state,
-             item_code, refferal_code, discount, offer_type, erp_code, payment_link)
+             item_code, refferal_code, discount, offer_type, erp_code, payment_link, mobile)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """, (
             unique_name, txn_id, amount, payable, course_id, email, name, firebase_uid,
             time, state, project, promoCode, discount, offerType,
-            erpCode, payment_link
+            erpCode, payment_link, mobile
         ))
         frappe.db.commit()
 
